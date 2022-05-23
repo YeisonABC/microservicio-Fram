c8cf9e3b12938ae98cd80a1fc552bc94b99f95a7
YeisonABC <yfbeltran8@misena.edu.co>
2022-05-23 15:29:43 -0500

    creacion del proyecto

---
+++ src/main/java/com/Entity/Cliente.java
@@ -0,0 +1,33 @@
+package com.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nombre;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
